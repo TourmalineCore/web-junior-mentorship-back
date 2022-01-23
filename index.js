aaa1fdef5cb4445452b405839c38155f84434df4
Katariniss <echekina@gmail.com>
2022-01-23 15:11:59 +0500

    chore(create): #3: create new client and add to clients

--- index.js
+++ index.js
@@ -8,31 +8,39 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+const clients = [
+  {
+    id: 1,
+    name: 'Bombaster'
+  },
+  {
+    id: 2,
+    name: 'Lowcoaster'
+  },
+  {
+    id: 3,
+    name: 'Joomanji'
+  },
+]
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/clients', (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: 'Bombaster'
-    },
-    {
-      id: 2,
-      name: 'Lowcoaster'
-    },
-    {
-      id: 3,
-      name: 'Joomanji'
-    },
-  ])
+  res.send(clients)
 })
 
 app.post('/clients', (req, res) => {
-  console.log(req.body);
+  const newClient = {
+    id: 4,
+    name: req.body.name,
+  }
+
+  clients.push(newClient)
+  
   res.send({
-    id: 4
+    id: newClient.id
   })
 })
 
