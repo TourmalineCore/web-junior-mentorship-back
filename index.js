3143cd691da71b62243a442bbae45993ae8f3522
Katariniss <echekina@gmail.com>
2022-01-23 15:03:39 +0500

    chore(create): #3: create ad-hock /clients POST

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 5000
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -24,6 +27,13 @@ app.get('/clients', (req, res) => {
       name: 'Joomanji'
     },
   ])
+})
+
+app.post('/clients', (req, res) => {
+  console.log(req.body);
+  res.send({
+    id: 4
+  })
 })
 
 app.listen(port, () => {
