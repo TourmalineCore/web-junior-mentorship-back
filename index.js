7a6e3a79f42607dfa7869dfe3e9ada638b7ebeb6
Katariniss <echekina@gmail.com>
2022-05-23 09:37:35 +0500

    feat(update): #11: add endpoint to update a particular client by id

--- index.js
+++ index.js
@@ -56,6 +56,27 @@ app.post(`/clients`, (req, res) => {
   })
 })
 
+app.post(`/clients/:id`, (req, res) => {
+  const clientId = parseInt(req.params.id)
+
+  const {
+    name,
+    description = ``, 
+  } = req.body
+
+  const client = clients.find(client => client.id === clientId)
+
+  if(client) {
+    client.name = name,
+    client.description = description
+    
+    res.send(true);
+  } else {
+    res.send(false);
+  }
+})
+
+
 app.delete(`/clients/:id`, (req, res) => {
   const clientId = parseInt(req.params.id);
 
